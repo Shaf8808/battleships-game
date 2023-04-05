7e1ce6b82645eee260b23b2d3e3ae2aa2e898c04
Shaf8808 <shadmankhan08@gmail.com>
2023-04-05 10:43:30 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+LENGTH_OF_SHIPS = [2, 3, 4, 5, 6]
+PLAYER_BOARD = [[" "] * 8 for x in range(8)]
+COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
+PLAYER_GUESS_BOARD = [[" "] * 8 for x in range(8)]
+COMPUTER_GUESS_BOARD = [[" "] * 8 for x in range(8)]
+LETTERS_TO_NUMBERS = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
+
